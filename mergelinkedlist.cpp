0f84bf512307800f15c37c77b600871881de49b5
Muhammad540 <thisisdeahmed@gmail.com>
2025-02-28 07:51:05 +0300

    Add mergedlinkedlist.cpp

---
+++ mergelinkedlist.cpp
@@ -0,0 +1,62 @@
+// we have two sorted ll, we need to merge them into one sorted ll
+// aim o(n+m) time and o(1) space
+#include <iostream>
+using namespace std;
+
+class ListNode{
+public:
+    int val;
+    ListNode* next;
+    ListNode(): val(0), next(nullptr){}
+    ListNode(int x): val(x), next(nullptr){}
+    ListNode(int x, ListNode* next): val(x), next(next){}   
+};
+
+class MergeLinkedList{
+    public:
+        ListNode* mergetwolists(ListNode* list1, ListNode* list2){
+            // head acts as a pointer to the head of the merged list
+            head = new ListNode();
+            // tail is like the pointer that traverses the merged list
+            tail = head;
+
+            while(list1 && list2){
+                if(list1->val < list2->val){
+                    tail->next = list1;
+                    list1 = list1->next;                    
+                } else{
+                    tail->next = list2;
+                    list2 = list2->next;
+                }
+                tail = tail->next;
+            }
+
+            // if any of the list is not null, we need to attach the remaining list to the tail
+            if(list1) tail->next = list1;
+            if(list2) tail->next = list2;
+
+            return head->next;
+        }
+        ~MergeLinkedList(){
+            delete head;
+            delete tail;
+        }
+        void printlist(ListNode* head){
+            while(head){
+                cout << head->val << " ";
+                head = head->next;
+            }
+        }
+    private:
+        ListNode* head;
+        ListNode* tail;
+};
+
+int main(){
+    MergeLinkedList ml;
+    ListNode* list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+    ListNode* list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+    ListNode* mergedList = ml.mergetwolists(list1, list2);
+    ml.printlist(mergedList);
+    return 0;
+}
